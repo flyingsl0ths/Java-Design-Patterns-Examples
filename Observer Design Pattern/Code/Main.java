819a66726dadc63dbf053ed98beff3938248eb42
flyingsl0ths <randomemail32110@gmail.com>
2020-02-26 17:48:30 -0600

    Updated file

--- Observer Design Pattern/Code/Main.java
+++ Observer Design Pattern/Code/Main.java
@@ -1,27 +1,50 @@
-public class Main {
-    public static void main(String[] args) {
-        StockGrabber stockGrabber = new StockGrabber();
-
-        StockObserver oberserver1 = new StockObserver(stockGrabber);
-
-        stockGrabber.setIBMPrice(197.00);
-        stockGrabber.setAAPLPrice(677.00);
-        stockGrabber.setGOOGPrice(677.00);
-
-        StockObserver oberserver2 = new StockObserver(stockGrabber);
-
-        stockGrabber.setIBMPrice(197.00);
-        stockGrabber.setAAPLPrice(677.60);
-        stockGrabber.setGOOGPrice(677.40);
-
-        stockGrabber.unregister(oberserver1);
-
-        Runnable getIBM = new GetTheStock(stockGrabber, 2, "IBM", 197.00);
-        Runnable getAAPL = new GetTheStock(stockGrabber, 2, "AAPL", 677.60);
-        Runnable getGOOG = new GetTheStock(stockGrabber, 2, "GOOG", 667.40);
-
-        new Thread(getIBM).start();
-        new Thread(getAAPL).start();
-        new Thread(getGOOG).start();
-    }   
+public class GrabStocks{
+	
+	public static void main(String[] args){
+		
+		// Create the Subject object
+		// It will handle updating all observers 
+		// as well as deleting and adding them
+		
+		StockGrabber stockGrabber = new StockGrabber();
+		
+		// Create an Observer that will be sent updates from Subject
+		
+		StockObserver observer1 = new StockObserver(stockGrabber);
+		
+		stockGrabber.setIBMPrice(197.00);
+		stockGrabber.setAAPLPrice(677.60);
+		stockGrabber.setGOOGPrice(676.40);
+		
+		StockObserver observer2 = new StockObserver(stockGrabber);
+		
+		stockGrabber.setIBMPrice(197.00);
+		stockGrabber.setAAPLPrice(677.60);
+		stockGrabber.setGOOGPrice(676.40);
+		
+		// Delete one of the observers
+		
+		// stockGrabber.unregister(observer2);
+		
+		stockGrabber.setIBMPrice(197.00);
+		stockGrabber.setAAPLPrice(677.60);
+		stockGrabber.setGOOGPrice(676.40);
+		
+		// Create 3 threads using the Runnable interface
+		// GetTheStock implements Runnable, so it doesn't waste 
+		// its one extendable class option
+		
+		Runnable getIBM = new GetTheStock(stockGrabber, 2, "IBM", 197.00);
+		Runnable getAAPL = new GetTheStock(stockGrabber, 2, "AAPL", 677.60);
+		Runnable getGOOG = new GetTheStock(stockGrabber, 2, "GOOG", 676.40);
+		
+		// Call for the code in run to execute 
+		
+		new Thread(getIBM).start();
+		new Thread(getAAPL).start();
+		new Thread(getGOOG).start();
+		
+		
+	}
+	
 }
